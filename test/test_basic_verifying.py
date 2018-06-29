ee8d222b76c36447e619cbee36d458687104e202
Martin Roy <mroy@internap.com>
2018-06-29 11:18:56 -0400

    Stub the 404
    Seems mock server has some problem with expected unexpected calls ;)

--- test/test_basic_verifying.py
+++ test/test_basic_verifying.py
@@ -5,6 +5,7 @@ from test import MOCK_SERVER_URL, MockServerClientTestCase
 
 class TestBasicVerifying(MockServerClientTestCase):
     def test_verify_request_received_once(self):
+        self.client.stub(request(), response())
         requests.get(MOCK_SERVER_URL)
         self.client.verify(request(), times(1))
 
